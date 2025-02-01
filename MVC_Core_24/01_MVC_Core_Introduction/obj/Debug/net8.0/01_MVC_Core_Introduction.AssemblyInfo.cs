a6be32f44e3b22f04d09c595cb6dd5ff32c2d037
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2025-02-01 19:23:38 +0530

    Cache in Server Side

--- MVC_Core_24/01_MVC_Core_Introduction/obj/Debug/net8.0/01_MVC_Core_Introduction.AssemblyInfo.cs
+++ MVC_Core_24/01_MVC_Core_Introduction/obj/Debug/net8.0/01_MVC_Core_Introduction.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01_MVC_Core_Introduction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56f9be215b7afe0e3dccddd2060ea4e5418d4005")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27a2c62838b0db0358010b8bff9113f87d359cf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("01_MVC_Core_Introduction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01_MVC_Core_Introduction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ MVC_Core_24/03_State_Management_Controller_To_View/Controllers/CacheController.cs
@@ -0,0 +1,77 @@
+﻿using _03_State_Management_Controller_To_View.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace _03_State_Management_Controller_To_View.Controllers
+{
+    public class CacheController : Controller
+    {
+        private readonly IMemoryCache _cache;
+        private readonly ILogger<CacheController> _logger;
+
+        public CacheController(IMemoryCache cache, ILogger<CacheController> logger)
+        {
+            _cache = cache;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string Name = string.Empty;
+
+            // Fetching Name from Cache
+            if (_cache.TryGetValue("name", out string value))
+            {
+                Name = value;
+                _logger.LogInformation("Name fetched from Cache");
+            }
+            else
+            {
+                Name = "Ajay Bhosle";
+                _cache.Set("name", Name, new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10) // Set expiration
+                });
+                _logger.LogInformation("Name fetched from Database");
+            }
+
+            // Fetching Customers from Cache
+            if (_cache.TryGetValue("customer", out List<Customer> customers))
+            {
+                ViewBag.Customers = customers;
+                _logger.LogInformation("Customers fetched from Cache");
+            }
+            else
+            {
+                List<Customer> customers1 = new List<Customer>()
+                {
+                    new Customer() { Name = "Ajay Bhosle", Age = 23, Email = "ajaybhosle.comp@gmail.com" },
+                    new Customer() { Name = "Vijay Bhosle", Age = 22, Email = "Vijay@gmail.com" }
+                };
+
+                _cache.Set("customer", customers1, new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10) // Set expiration
+                });
+
+                ViewBag.Customers = customers1;
+                _logger.LogInformation("Customers fetched from Database");
+            }
+
+            return View((object)Name);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
